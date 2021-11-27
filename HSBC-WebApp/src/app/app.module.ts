c5095d93fd941ca4b258db9a762d1b9caccda613
fsoljan <filip.soljan@gmail.com>
2021-11-27 10:47:12 +0100

    Added home screen

--- HSBC-WebApp/src/app/app.module.ts
+++ HSBC-WebApp/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatIconModule, MatSelectModule, MatCheckboxModule } from '@angular/material';
+import { SearchResultComponent } from './search-result/search-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    HomeComponent,
+    SearchComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule, 
+    FormsModule, 
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ HSBC-WebApp/src/app/search-result/search-result.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.css']
+})
+export class SearchResultComponent implements OnInit {
+  atms = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
