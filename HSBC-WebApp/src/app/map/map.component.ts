bc1f2ecdc9046480b23c83bf1a9a1e582e961d26
fsoljan <filip.soljan@gmail.com>
2021-11-28 22:19:54 +0100

    Added marker to each feature on map

--- HSBC-WebApp/src/app/map/map.component.ts
+++ HSBC-WebApp/src/app/map/map.component.ts
@@ -2,10 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import VectorSource from 'ol/source/Vector';
 import OSM from 'ol/source/OSM';
 import { DataService } from '../services/data.service';
 import { AtmModel } from '../models/atm.model';
+import { Feature } from 'ol';
+import { Icon, Style } from 'ol/style';
+import { fromLonLat } from 'ol/proj.js';
+import Point from 'ol/geom/Point';
 
 @Component({
   selector: 'app-map',
@@ -13,25 +18,54 @@ import { AtmModel } from '../models/atm.model';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  atms: AtmModel[];
   map: Map;
   id = -1;
 
   constructor(private routes: ActivatedRoute, private dataService: DataService) { }
 
   ngOnInit(): void {
-    this.routes.params.subscribe(params => {
-      this.id = params.id
-      if (this.id === undefined)
-        this.initMap();
-      else {
-        this.dataService.getAtm(this.id).subscribe(data => {
-          this.initMap(data.geoLocation[0], data.geoLocation[1], 18);
-        });
-      }
+    this.dataService.getAllAtms().subscribe(data => {
+      this.atms = data;
+      this.routes.params.subscribe(params => {
+        this.id = params.id
+        if (this.id === undefined)
+          this.initMap();
+        else {
+          this.dataService.getAtm(this.id).subscribe(data => {
+            this.initMap(data.geoLocation[0], data.geoLocation[1], 18);
+          });
+        }
+      });
     });
   }
 
   initMap(x: number = 1780206, y: number = 5749983, zoom = 12) {
+    let vectorSource = new VectorSource({
+      features: []
+    });
+
+    for (let atm of this.atms) {
+      let atmFeature = new Feature({
+        geometry: new Point(atm.geoLocation)
+      });
+
+      atmFeature.setStyle(new Style({
+        image: new Icon(({
+          color: '#8959A8',
+          crossOrigin: 'anonymous',
+          src: 'assets/images/pin.png',
+          imgSize: [20, 20]
+        }))
+      }));
+      
+      vectorSource.addFeature(atmFeature);
+    }
+
+    let vectorLayer = new VectorLayer({
+      source: vectorSource
+    });
+
     this.map = new Map({
       view: new View({
         center: [x, y],
@@ -41,6 +75,7 @@ export class MapComponent implements OnInit {
         new TileLayer({
           source: new OSM(),
         }),
+        vectorLayer
       ],
       target: 'ol-map'
     });
